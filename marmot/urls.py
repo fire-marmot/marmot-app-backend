0393e8644956cea1edbdd74b9907dafb73273c5c
Chris Rarig <chrisrar@zillowgroup.com>
2022-05-16 17:20:23 -0500

    added urls

--- marmot/urls.py
+++ marmot/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import MoviesList, MoviesDetail
+from .views import MoviesList, MoviesDetail, UserList,UserDetail
 
 urlpatterns = [
     path("", MoviesList.as_view(), name="movies_list"),
     path("<int:pk>/", MoviesDetail.as_view(), name="movies_detail"),
+    path("user", UserList.as_view(), name="user_list"),
+    path("user/<int:pk>/", UserDetail.as_view(), name="user_detail"),
 ]
